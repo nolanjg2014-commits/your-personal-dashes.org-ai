cb61de05ef8a96c6ed8332344019ce9b532a4c57
nolanjg2014-commits <nolanjg2014@gmail.com>
2026-06-17 17:07:54 -0700

    Add files via upload

---
+++ ai chatbot.py
@@ -0,0 +1,76 @@
+print("welcome to your personal dash's chat bot assistant what do you need from the dash")
+print("this is a short cut to all the things on this dash")
+
+while True:
+  print("what do you need? (Type 'exit' to quit)")
+  user_to_bot = input(f"user: ")
+
+  if user_to_bot == "exit":
+    print("Goodbye!")
+    break
+  elif user_to_bot == "gouncover":
+    print("https://sites.google.com/view/backround47/home need anything else?")
+  elif user_to_bot == "gmail":
+    print("https://mail.google.com/ need anything else?")
+  elif user_to_bot == "minecraft help":
+    print("https://docs.google.com/spreadsheets/d/e/2PACX-1vR5O3Oe6MsNTAhIySMcSEWkYBdqL7EDwTt3KghKSIXZWpRAhHhP81dr1cmIMSKPhek7SfXXl39ZLghW/pubhtml need anything else?")
+  elif user_to_bot == "canva":
+    print("https://canva.com/ need anything else?")
+  elif user_to_bot == "youtube":
+    print("https://youtube.com/ need anything else?")
+  elif user_to_bot == "discord":
+    print("https://discord.com/ need anything else?")
+  elif user_to_bot == "geoseer":
+    print("https://geoseeer.com/ need anything else?")
+  elif user_to_bot == "twitter":
+    print("https://twitter.com/ need anything else?")
+  elif user_to_bot == "instagram":
+    print("https://instagram.com/ need anything else?")
+  elif user_to_bot == "netflix":
+    print("https://netflix.com/ need anything else?")
+  elif user_to_bot == "spotify":
+    print("https://spotify.com/ need anything else?")
+  elif user_to_bot == "amazon":
+    print("https://amazon.com/ need anything else?")
+  elif user_to_bot == "colab":
+    print("https://research.google.com/ need anything else?")
+  elif user_to_bot == "gemini":
+    print("https://gemini.google.com/ need anything else?")
+  elif user_to_bot == "calculator":
+    print("https://www.theonlinecalculator.com/ need anything else?")
+  elif user_to_bot == "backround remover":
+    print("https://www.adobe.com/express/feature/image/remove-background need anything else?")
+  elif user_to_bot == "photopea":
+    print("https://photopea.com/ need anything else?")
+  elif user_to_bot == "microsoft":
+    print("https://microsoft.com/ need anything else?")
+  elif user_to_bot == "outlook":
+    print("https://outlook.com/ need anything else?")
+  elif user_to_bot == "pyscript":
+    print("https://pyscript.com/ need anything else?")
+  elif user_to_bot == "addictinggames":
+    print("https://addictinggames.com/ need anything else?")
+  elif user_to_bot == "tiny fishing":
+    print("https://tiny-fishing.bitbucket.io/ need anything else?")
+  elif user_to_bot == "geforce":
+    print("https://play.geforcenow.com/mall/ need anything else?")
+  elif user_to_bot == "new tiny fishing":
+    print("https://bitlife-pro.github.io/ need anything else?")
+  elif user_to_bot == "xbox":
+    print("https://xbox.com/ need anything else?")
+  elif user_to_bot == "steam":
+    print("https://store.steampowered.com/ need anything else?")
+  elif user_to_bot == "neal.fun":
+    print("https://neal.fun/ need anything else?")
+  elif user_to_bot == "geofs":
+    print("https://geofs.com/ need anything else?")
+  elif user_to_bot == "ztype":
+    print("https://ztype.com/ need anything else?")
+  elif user_to_bot == "slow roads":
+    print("https://slowroads.io/ need anything else?")
+  elif user_to_bot == "fps":
+    print("https://sites.google.com/view/yourpersonaldash/fps need anything else?")
+  elif user_to_bot == "dash":
+    print("https://www.sites.google.com/view/yourpersonaldash/acessgranted")
+  else:
+    print(f"Sorry, I don't recognize '{user_to_bot}'. Please try again or type 'exit'.")
